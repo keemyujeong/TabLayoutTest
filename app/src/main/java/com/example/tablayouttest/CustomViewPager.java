fbe06d1b1104824f9ddbc0859f534059e44683db
cheonjinho <fvwlharu@naver.com>
2023-02-24 16:02:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tablayouttest/CustomViewPager.java
@@ -0,0 +1,30 @@
+package com.example.tablayouttest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+public class CustomViewPager extends ViewPager {
+
+    public CustomViewPager(Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false; // 스와이프 방지
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false; // 스와이프 방지
+    }
+}
---
+++ app/src/main/java/com/example/tablayouttest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.tablayouttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tablayouttest.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
